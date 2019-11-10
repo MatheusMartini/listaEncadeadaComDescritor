e0d7e0bec2b0ddc2dd383d232fdb142c9d9add5b
MatheusMartini <matheus.martini@rede.ulbra.br>
2019-11-10 19:39:10 -0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 import java.util.Scanner;
 
+/*
+1- Incluir no final
+2 - Incluir no início
+3- Incluir após um nome(ler um nome, encontrar na lista e incluir na posição posterior)
+4 - Consultar por nome (ler um nome e mostrar o registro do nome procurado)
+5 - Excluir do início
+6 - Excluir do fim
+7 - Excluir por código
+8 - Listar todos os registros
+9 - Listar com médias finais ( listar os nomes dos alunos com suas médias finais calculadas)
+10- Sair
+ */
+
 public class Main {
     public static void main(String[] args) {
 
